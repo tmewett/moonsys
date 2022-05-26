166481f1c24141dad718a5b44d342e61bfb1e211
Tom M <tom@collider.in>
2022-05-26 19:42:41 +0100

    add builtin types, struct

--- ldata.lua
+++ ldata.lua
@@ -10,7 +10,31 @@ M.proto = {
     end
 }
 
-function M.type(x)
+local function builtin_type(s)
+    return {
+        _test = function (x)
+            return type(x) == s
+        end
+    }
+end
+
+M['string'] = builtin_type('string')
+M['table'] = builtin_type('table')
+M['number'] = builtin_type('number')
+M['function'] = builtin_type('function')
+
+function M.is_type(x, t)
+    return t:_test(x)
+end
+
+function M.struct(desc)
+    return function (args)
+        for name, type in pairs(desc) do
+            if M.is_type(args[name], type) then
+                error("wrong type")
+            end
+        end
+    end
 end
 
 return M
