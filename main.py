56f57dcb6ddda62328899ca0b789aa73e4fe06af
Tom M <tom@tmewett.com>
2022-12-16 22:49:36 +0000

    python, it doesn't work???

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ctypes
 from array import array
+from dataclasses import dataclass
 
 import pyglet
 from pyglet.graphics.vertexbuffer import BufferObject
@@ -8,7 +9,7 @@ from pyglet.gl import *
 
 _keys = dict()
 
-def array_bytelen(a):
+def array_sizeof(a):
     return a.buffer_info()[1] * a.itemsize
 
 def array_ptr(a):
@@ -22,111 +23,83 @@ def use_memo(key, f):
     _keys[key] = x
     return x
 
-def use_program(**types):
-    shaders = [
-        use_memo(src, lambda: Shader(src, type))
-        for type, src in types.items()
-    ]
-    return use_memo(frozenset(types.values()), lambda: ShaderProgram(*shaders))
+def use_program(**stages):
+    def new():
+        shaders = [
+            use_memo(src, lambda: Shader(src, stage))
+            for stage, src in stages.items()
+        ]
+        print(stages)
+        return ShaderProgram(*shaders)
+    return use_memo(frozenset(stages.values()), new)
 
 def _to_buffer(data):
-    buf = BufferObject(array_bytelen(data))
-    buf.set_data(data.tobytes())
+    buf = BufferObject(array_sizeof(data))
+    # buf.set_data(data.tobytes())
+    glBufferData(GL_ARRAY_BUFFER, array_sizeof(data), array_ptr(data), GL_DYNAMIC_DRAW)
     return buf
 
-def use_attributes(spec, data):
-    buf = use_memo(id(data), lambda: _to_buffer(data))
+@dataclass
+class VAO():
+    id: int
+    spec: list
+
+def new_attributes(spec, data):
+    buf = _to_buffer(data)
+    buf.bind(GL_ARRAY_BUFFER)
     vao = GLuint()
     glGenVertexArrays(1, vao)
     glBindVertexArray(vao)
-    buf.bind(GL_ARRAY_BUFFER)
     stride = sum(s[1] for s in spec)
     offset = 0
     for i, (name, vlen) in enumerate(spec):
+        print(i, vlen, GL_FLOAT, False, stride * ctypes.sizeof(GLfloat), offset * ctypes.sizeof(GLfloat))
         glVertexAttribPointer(i, vlen, GL_FLOAT, False, stride * ctypes.sizeof(GLfloat), offset * ctypes.sizeof(GLfloat))
-        glEnableVertexAttribArray(0)
+        glEnableVertexAttribArray(i)
         offset += vlen
+    return VAO(id=vao, spec=spec)
 
-
-def set_state(**opts):
-
-
-# def draw_arrays(prim, start, n)
-
-# def use_buffer(data, *, key=None):
-#     key = key if key is not None else data
-#     def new():
-#         return pyglet.graphics.BufferObject()
+def set_state(*, attributes, vertex_shader, fragment_shader):
+    vertex_src = "\n".join([
+        "#version 330 core",
+        *[f"layout(location={loc}) in vec{n} {name};" for loc, (name, n) in enumerate(attributes.spec)],
+        vertex_shader,
+    ])
+    fragment_src = "\n".join([
+        "#version 330 core",
+        fragment_shader,
+    ])
+    program = use_program(vertex=vertex_src, fragment=fragment_src)
+    program.use()
 
 window = pyglet.window.Window()
 
-vertices = array('f', [
-    -0.5, -0.5, 0.0, 1.0,
-     0.5, -0.5, 0.0, 1.0,
-     0.0,  0.5, 0.0, 1.0,
-])
-
-buf = GLuint()
-glGenBuffers(1, buf)
-glBindBuffer(GL_ARRAY_BUFFER, buf)
-glBufferData(GL_ARRAY_BUFFER, array_bytelen(vertices), array_ptr(vertices), GL_STATIC_DRAW)
-
-vao = GLuint()
-glGenVertexArrays(1, vao)
-glBindVertexArray(vao)
-glVertexAttribPointer(0, 4, GL_FLOAT, False, 0, 0)
-glEnableVertexAttribArray(0)
-
-# vertex_s = glCreateShader(GL_VERTEX_SHADER)
-# glShaderSource(vertex_s, 1, (ctypes.c_char_p * 1)(ctypes.c_char_p("""
-# #version 330 core
-# layout(location=0) in vec2 position;
-# void main()
-# {
-#     gl_Position = vec4(position, 0.0, 1.0);
-# }
-# """.encode())), None)
-# glCompileShader(vertex_s)
-
-# frag_s = glCreateShader(GL_FRAGMENT_SHADER)
-# glShaderSource(frag_s, 1, (ctypes.c_char_p * 1)(ctypes.c_char_p("""
-# #version 330 core
-# out vec4 outColor;
-# void main()
-# {
-#     outColor = vec4(1.0, 1.0, 1.0, 1.0);
-# }
-# """.encode())), None)
-# glCompileShader(frag_s)
-
-# shaderProgram = glCreateProgram()
-# glAttachShader(shaderProgram, vertex_s)
-# glAttachShader(shaderProgram, frag_s)
-# # glBindFragDataLocation(shaderProgram, 0, "outColor")
-# glLinkProgram(shaderProgram)
-# glUseProgram(shaderProgram)
+attributes = new_attributes(
+    [('pos', 4)],
+    array('f', [
+        -0.5, -0.5, 0.0, 1.0,
+        0.5, -0.5, 0.0, 1.0,
+        0.0,  0.5, 0.0, 1.0,
+    ]),
+)
 
 @window.event
 def on_draw():
     window.clear()
-    use_program(
-        vertex="""
-            #version 330 core
-            layout(location=0) in vec2 position;
-            void main()
-            {
-                gl_Position = vec4(position, 0.0, 1.0);
+    set_state(
+        attributes=attributes,
+        vertex_shader="""
+            void main() {
+                gl_Position = pos;
             }
         """,
-        fragment="""
-            #version 330 core
+        fragment_shader="""
             out vec4 outColor;
-            void main()
-            {
+            void main() {
                 outColor = vec4(1.0, 1.0, 1.0, 1.0);
             }
         """,
-    ).use()
+    )
     glDrawArrays(GL_TRIANGLES, 0, 3)
 
 pyglet.app.run()
