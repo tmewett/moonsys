699f526f80dc061658e037d07f2f07b0cb3d5c74
Tom M <tom@tmewett.com>
2023-04-05 14:32:21 +0100

    more react.py; _define_props, implement hooks, Context, Memo

--- react.py
+++ react.py
@@ -1,23 +1,35 @@
+from inspect import signature
+
 class Component:
-    def __init__(self, **props):
-        self.props = props
-    def set_props(self):
+    def __init__(self, *a, **kw):
+        self.set_props(*a, **kw)
+    def set_props(self, *a, **kw):
+        # self.__class__._define_props(*a, **kw)
+        self.props = signature(self.__class__._define_props).bind(*a, **kw).arguments
+    def do(self):
         pass
     def refresh(self):
+        self.undo()
+        self.do()
+    def undo(self):
         pass
 
 class Debug(Component):
+    def set_props(self, **props):
+        self.props = props
     def do(self):
-        print(f"  do {id(self)} props={self.props}")
+        print(f"     do {id(self)} props={self.props}")
+    def refresh(self):
+        print(f"refresh {id(self)} props={self.props}")
     def undo(self):
-        print(f"undo {id(self)} props={self.props}")
+        print(f"   undo {id(self)} props={self.props}")
 
 class DelegatedComponent(Component):
     def do(self):
-        self.setup()
         self._slots = []
         self._slot_i = None
         self._delegate = self.get_children()
+        self._delegate.parent = self
         self._delegate.do()
     def refresh(self):
         self._slot_i = 0
@@ -27,51 +39,81 @@ class DelegatedComponent(Component):
             self._delegate.refresh()
         else:
             self._delegate.undo()
-            new.do()
             self._delegate = new
+            self._delegate.parent = self
+            self._delegate.do()
     def undo(self):
         self._delegate.undo()
         for h in reversed(self._slots):
             h.undo()
     def use(self, c, **props):
-        def get_value(x):
-            nonlocal value
-            value = x
-        use_props = props | {'then': get_value}
         if self._slot_i is None:
-            self._slots.append(c(**use_props))
+            self._slots.append(c(**props))
             self._slots[-1].do()
-            return value
+            return self._slots[-1].get_value()
         else:
             old = self._slots[self._slot_i]
             if not issubclass(c, type(old)):
                 raise RuntimeError()
-            old.props = use_props
+            old.props = props
             old.refresh()
             self._slot_i += 1
-        return value
+            return old.get_value()
 
-class Memo(DelegatedComponent):
+class With(DelegatedComponent):
+    def _define_props(component, then):
+        pass
     def do(self):
-        self._last_deps = None
-        self._value = None
+        self._component = self.props['component']
+        self._component.do()
         super().do()
+    def refresh(self):
+        new = self.props['component']
+        if isinstance(new, type(self._component)):
+            self._component.props = new.props
+            self._component.refresh()
+        else:
+            self._component.undo()
+            new.do()
+            self._component = new
+        super().refresh()
+    def undo(self):
+        super().undo()
+        self._component.undo()
     def get_children(self):
-        if self._last_deps != self.props['deps']:
-            self._value = self.props['f']()
-        return self.props['then'](self._value)
+        return self.props['then'](self._component.get_value())
 
-def use_memo(f, deps):
-    if len(_current_component._slots) == _current_component._next_key:
-        store = {'value':None, 'deps':None}
-        _current_component._slots.append(store)
+class Memo(Component):
+    def _define_props(f, deps=None):
+        pass
+    def do(self):
+        self._last_deps = self.props['deps']
+        self._value = self.props['f']()
+    # TODO refresh
+    def get_value(self):
+        if self.props['deps'] is not None and self._last_deps != self.props['deps']:
+            self._last_deps = self.props['deps']
+            self._value = self.props['f']()
+            print("reran memo")
+        return self._value
+
+class Provider(DelegatedComponent):
+    def _define_props(key, value, children):
+        pass
+    def get_children(self):
+        return self.props['children']
+
+def _find_provider(key, c):
+    if isinstance(c, Provider) and c.props['key'] == key:
+        return c.props['value']
     else:
-        store = _current_component._slots[_current_component._next_key]
-    _current_component._next_key += 1
-    if deps is None or deps != store['deps']:
-        store['value'] = f()
-        store['deps'] = deps
-    return store['value']
+        return _find_provider(key, c.parent)
+
+class Context(Component):
+    def _define_props(key, *, default=None):
+        pass
+    def get_value(self):
+        return _find_provider(self.props['key'], self.parent)
 
 class Ref:
     def __init__(self, value, element):
@@ -83,14 +125,14 @@ class Ref:
         self._value = x
         self._element.refresh()
 
-def use_state(initial):
-    return use_memo(lambda: Ref(initial, _current_component))
-
+# TODO No, each f needs to be different class.
 class FunctionComponent(DelegatedComponent):
     _current = None
-    def __init__(self, f, props):
-        super().__init__(**props)
+    def __init__(self, f, *a, **kw):
         self._function = f
+        super().__init__(*a, **kw)
+    def set_props(self, *a, **kw):
+        self.props = signature(self._function).bind(*a, **kw).arguments
     def get_children(self):
         FunctionComponent._current = self
         c = self._function(**self.props)
@@ -98,17 +140,20 @@ class FunctionComponent(DelegatedComponent):
         return c
 
 def component(f):
-    return lambda **props: FunctionComponent(f, props)
+    return lambda *a, **kw: FunctionComponent(f, *a, **kw)
 
 def use(c, **props):
     return FunctionComponent._current.use(c, **props)
 
 @component
-def func(id):
-    return Debug(id=id)
+def func(n):
+    nn = use(Memo, f=lambda: n*2, deps=[n])
+    return Debug(n=nn)
 
-f = func(id=1)
+f = func(n=1)
 f.do()
-f.props['id'] = 2
+f.props['n'] = 1
+f.refresh()
+f.props['n'] = 2
 f.refresh()
 f.undo()
